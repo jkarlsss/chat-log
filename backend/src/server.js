00adecf96717d29e153d2273df9cc3b120298d66
jkarlsss <kolley320@gmail.com>
2025-09-17 23:03:53 +0800

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,28 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import authRoutes from './routes/auth.js';
-import messageRoutes from './routes/message.js';
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+
+import authRoutes from "./routes/auth.js";
+import messageRoutes from "./routes/message.js";
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 5000;
 
-app.use('/api/auth', authRoutes);
-app.use('/api/messages', messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("/*path", (_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+};
 
 app.listen(PORT, () => 
     console.log(`Server is running on http://localhost:${PORT}`)
