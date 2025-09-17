d1ab6a3bc39cbb50ee53af50406f93dfb7e3c01c
jkarlsss <kolley320@gmail.com>
2025-09-18 01:29:28 +0800

    revised package json.

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,19 +1,15 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
-
-router.get('/signup', (req, res) => {
-    res.send('Signup endpoint');
+router.get("/signup", (req, res) => {
+    res.send("Signup Page");
 });
-
-router.get('/login', (req, res) => {
-    res.send('Login endpoint');
+router.get("/login", (req, res) => {
+    res.send("Login Page");
 });
-
-router.get('/logout', (req, res) => {
-    res.send('Logout endpoint');
+router.get("/logout", (req, res) => {
+    res.send("Logout Page");
 });
-
 
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,27 +3,23 @@ import dotenv from "dotenv";
 import path from "path";
 
 import authRoutes from "./routes/auth.js";
-import messageRoutes from "./routes/message.js";
 
 dotenv.config();
 
-const app = express();
 const __dirname = path.resolve();
-
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
 
-// Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("/*path", (_, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
     });
-};
+}
 
-app.listen(PORT, () => 
-    console.log(`Server is running on http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
